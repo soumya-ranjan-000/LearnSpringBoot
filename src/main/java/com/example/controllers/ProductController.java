d95c000c37c862a1ea2698f8437feb7b5b8ebc4f
soumya-ranjan-000 <58003666+soumya-ranjan-000@users.noreply.github.com>
2024-11-01 18:15:34 +0000

    added one product rest service

---
+++ src/main/java/com/example/controllers/ProductController.java
@@ -0,0 +1,21 @@
+package com.example.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.services.Products;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    private Products products;
+    
+    @RequestMapping("/products")
+    @ResponseBody
+    public String getProducts(){
+        return products.getProducts();
+    }
+}
---
+++ src/main/java/com/example/data/Product.java
@@ -0,0 +1,35 @@
+package com.example.data;
+
+public class Product {
+    
+    private String name;
+    private Double price;
+    private Integer quantity;
+
+
+    public Product(String name, double price, Integer quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Double getPrice() {
+        return price;
+    }
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+    public Integer getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    
+}
